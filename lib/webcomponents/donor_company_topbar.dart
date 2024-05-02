8d8a72fcc08033a231ab974e64fc37b9ba3c0df6
jep180001 <112898626+jep180001@users.noreply.github.com>
2024-05-02 16:37:18 -0500

    Update donor_company_topbar.dart

--- lib/webcomponents/donor_company_topbar.dart
+++ lib/webcomponents/donor_company_topbar.dart
@@ -4,7 +4,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 import '../authentication/auth.dart';
 import '../webpages/company_donor/donationofinterestspage.dart';
 import '../webpages/company_donor/donorcompanydonationhistory.dart';
