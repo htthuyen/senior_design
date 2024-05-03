60a1369857b3862148c8c292a47dd4063ba1a63b
jep180001 <112898626+jep180001@users.noreply.github.com>
2024-05-02 20:29:24 -0500

    Update donorprofile.dart

--- lib/webpages/company_donor/donorprofile.dart
+++ lib/webpages/company_donor/donorprofile.dart
@@ -151,17 +151,7 @@ class _DonorProfilePageState extends State<DonorProfilePage> {
                       }, 
                       icon: const Icon(Icons.edit),
                     ),
-                    IconButton(
-                      icon: Icon(
-                        isFavorite ? Icons.favorite : Icons.favorite_border,
-                        color: isFavorite ? Colors.red : null,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          isFavorite = !isFavorite;
-                        });
-                      },
-                    ),
+                    
                     SizedBox(width: 10),
                     IconButton(
                       icon: Icon(Icons.attach_money),
@@ -294,6 +284,14 @@ void _showEditProfileDialog(BuildContext context) {
       _database.child('users').child(currentUserUid).update(updateData)
         .then((_) {
           print('User information updated successfully.');
+          setState(() {
+            
+            name = newName;
+            email = newEmail;
+            aboutUs = newAboutUs;
+            website = newWebsite;
+          
+          });
         })
         .catchError((error) {
           print('Error updating user information: $error');
